d2a15e8b5576a6287da89981edb4b5b17bd483cb
Kiem Nguyen <kiemnguyen@gmail.com>
2014-09-08 19:55:10 -0500

    Initial commit

---
+++ COUNTING VOWELS.py
@@ -0,0 +1,15 @@
+#Assume s is a string of lower case characters.
+
+#Write a program that counts up the number of vowels contained in the string s. Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. For example, if , your program should print:
+
+#Number of vowels: 5
+
+s = str(raw_input("Enter a string: " ));
+def vowels(string):
+    count = 0
+    for i in range (len(s)):
+        if string[i] == 'a' or string[i] == 'e' or string[i] == 'i' or string[i] == 'o' or \
+           string[i] == 'u':
+           count = count + 1
+    return count
+print("Number of vowels: " + str(vowels(s)))
